12972b5983bb99456677614d5e7dfdb335f77cb7
Bharadwaj P <barbi.bruce@gmail.com>
2011-03-26 05:21:07 -0500

    admin login form, site setting form

---
+++ admin/login.php
@@ -0,0 +1,15 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<title>Vallpress Admin Login</title>
+	
+</head>
+
+<body>
+<form action="" method="POST" accept-charset="utf-8">
+    <label for="username">Username</label><input type="text" name="username" value="" id="username"/>
+    <label for="password">Password</label><input type="password" name="password" value="" id="password"/>
+    <p><input type="submit" value="Login"></p>
+</form>
+
+</body>
+</html>
